4a29db18fd6828945b70e0677f955cb31aefa8aa
Andrey Yusupov <electrooptic@yandex.ru>
2018-12-17 18:07:21 +0700

    Add Airport

---
+++ FindAviaTicket/Airport.m
@@ -0,0 +1,35 @@
+//
+//  Airport.m
+//  FindAviaTicket
+//
+//  Created by Andrey Yusupov on 17/12/2018.
+//  Copyright © 2018 Andrey Yusupov. All rights reserved.
+//
+
+#import "Airport.h"
+
+@implementation Airport
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        _name = [dictionary valueForKey:@"name"];
+        _code = [dictionary valueForKey:@"code"];
+        _countryCode = [dictionary valueForKey:@"country_code"];
+        _cityCode = [dictionary valueForKey:@"city_code"];
+        _timezone = [dictionary valueForKey:@"time_zone"];
+        _translations = [dictionary valueForKey:@"name_translations"];
+        _flightable = [dictionary valueForKey:@"flightable"];
+        NSDictionary *coords = [dictionary valueForKey:@"coordinates"];
+        if (coords && ![coords isEqual:[NSNull null]]) {
+            NSNumber *lat = [coords valueForKey:@"lat"];
+            NSNumber *lon = [coords valueForKey:@"lon"];
+            if (![lat isEqual:[NSNull null]] && ![lon isEqual:[NSNull null]]) {
+                _coordinate = CLLocationCoordinate2DMake([lat doubleValue], [lon doubleValue]);
+            }
+        }
+    }
+    return self;
+}
+
+@end
