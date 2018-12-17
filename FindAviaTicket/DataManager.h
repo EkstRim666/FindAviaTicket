30396f5f711c494952facfa932f9345fdc3edcfa
Andrey Yusupov <electrooptic@yandex.ru>
2018-12-17 20:04:06 +0700

    Add DataManager

---
+++ FindAviaTicket/DataManager.h
@@ -0,0 +1,35 @@
+//
+//  DataManager.h
+//  FindAviaTicket
+//
+//  Created by Andrey Yusupov on 17/12/2018.
+//  Copyright © 2018 Andrey Yusupov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Airport.h"
+#import "City.h"
+#import "Country.h"
+
+#define dataManagerLoadDataDidComplete @"DataManagerLoadDataDidComplete"
+
+typedef enum DataSourceType {
+    DataSourceTypeCountry,
+    DataSourceTypeCity,
+    DataSourceTypeAirport
+} DataSourceType;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataManager : NSObject
+
+@property (strong, nonatomic) NSArray<Airport *> *airports;
+@property (strong, nonatomic) NSArray<City *> *cities;
+@property (strong, nonatomic) NSArray<Country *> *countries;
+
++(instancetype)sharedInstance;
+-(void)loadData;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FindAviaTicket/DataManager.m
@@ -0,0 +1,73 @@
+//
+//  DataManager.m
+//  FindAviaTicket
+//
+//  Created by Andrey Yusupov on 17/12/2018.
+//  Copyright © 2018 Andrey Yusupov. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
++(instancetype)sharedInstance {
+    static DataManager *instance;
+    static dispatch_once_t dataManagerOnceToken;
+    dispatch_once(&dataManagerOnceToken, ^{
+        instance = [DataManager new];
+    });
+    return instance;
+}
+
+-(NSArray *)arrayFromFileName:(NSString *)fileName
+                     ofType:(NSString *)type
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:fileName ofType:type];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    return [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+}
+
+-(NSArray *)createObjectsFromArray:(NSArray *)array
+                          withType:(DataSourceType)type
+{
+    NSMutableArray *results = [NSMutableArray new];
+    for (NSDictionary *jsonObject in array) {
+        switch (type) {
+            case DataSourceTypeCountry: {
+                Country *country = [[Country alloc] initWithDictionary:jsonObject];
+                [results addObject:country];
+                break;
+            }
+            case DataSourceTypeCity: {
+                City *city = [[City alloc] initWithDictionary:jsonObject];
+                [results addObject:city];
+                break;
+            }
+            case DataSourceTypeAirport: {
+                Airport *airport = [[Airport alloc] initWithDictionary:jsonObject];
+                [results addObject:airport];
+                break;
+            }
+        }
+    }
+    return [NSArray arrayWithArray:results];
+}
+
+-(void)loadData {
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0), ^{
+        NSArray *countriesJsonArray = [self arrayFromFileName:@"contries" ofType:@"json"];
+        self->_countries = [self createObjectsFromArray:countriesJsonArray withType: DataSourceTypeCountry];
+        
+        NSArray *citiesJsonArray = [self arrayFromFileName:@"cities" ofType:@"json"];
+        self->_cities = [self createObjectsFromArray:citiesJsonArray withType: DataSourceTypeCity];
+        
+        NSArray *airportsJsonArray = [self arrayFromFileName:@"airports" ofType:@"json"];
+        self->_airports = [self createObjectsFromArray:airportsJsonArray withType: DataSourceTypeAirport];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[NSNotificationCenter defaultCenter] postNotificationName:dataManagerLoadDataDidComplete object:nil];
+        });
+    });
+}
+
+@end
