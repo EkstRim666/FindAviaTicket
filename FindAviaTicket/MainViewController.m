2d0f244842548063fc59d45c415bcbde89d64b35
Andrey Yusupov <electrooptic@yandex.ru>
2018-12-21 12:28:56 +0700

    Add placeViewController in MainViewConroller

--- FindAviaTicket/MainViewController.m
+++ FindAviaTicket/MainViewController.m
@@ -16,6 +16,7 @@
 
 @interface MainViewController ()<PlaceTableViewControllerDelegate>
 
+@property (strong, nonatomic) UIView *placeContainerView;
 @property (strong, nonatomic) UIButton *departureButton;
 @property (strong, nonatomic) UIButton *arrivalButton;
 
@@ -54,23 +55,34 @@
     self.navigationController.navigationBar.prefersLargeTitles = YES;
     self.title = @"Search";
     
+    self.placeContainerView = [[UIView alloc] initWithFrame:CGRectMake(20, (CGRectGetMaxY(self.navigationController.navigationBar.frame) + 40), (UIScreen.mainScreen.bounds.size.width - 40), 170)];
+    self.placeContainerView.backgroundColor = UIColor.whiteColor;
+    self.placeContainerView.layer.shadowColor = [[UIColor.blackColor colorWithAlphaComponent:0.1] CGColor];
+    self.placeContainerView.layer.shadowOffset = CGSizeZero;
+    self.placeContainerView.layer.shadowRadius = 20;
+    self.placeContainerView.layer.shadowOpacity = 1;
+    self.placeContainerView.layer.cornerRadius = 6;
+    [self.view addSubview:self.placeContainerView];
+    
     self.departureButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.departureButton setTitle:@"Departure" forState:UIControlStateNormal];
     self.departureButton.tintColor = UIColor.blackColor;
-    self.departureButton.frame = CGRectMake(30, (CGRectGetMaxY(self.navigationController.navigationBar.frame) + 40), (UIScreen.mainScreen.bounds.size.width - 60), 60);
+    self.departureButton.frame = CGRectMake(10, 20, (self.placeContainerView.frame.size.width - 20), 60);
     self.departureButton.backgroundColor = [UIColor.lightGrayColor colorWithAlphaComponent:0.3];
+    self.departureButton.layer.cornerRadius = 4;
     [self.departureButton addTarget:self action:@selector(placeButtonDidTap:) forControlEvents:UIControlEventTouchUpInside];
     [self.departureButton setEnabled:NO];
-    [self.view addSubview:self.departureButton];
+    [self.placeContainerView addSubview:self.departureButton];
     
     self.arrivalButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.arrivalButton setTitle:@"Arrival" forState:UIControlStateNormal];
     self.arrivalButton.tintColor = UIColor.blackColor;
-    self.arrivalButton.frame = CGRectMake(30, (CGRectGetMaxY(self.departureButton.frame) + 20), (UIScreen.mainScreen.bounds.size.width - 60), 60);
+    self.arrivalButton.frame = CGRectMake(10, (CGRectGetMaxY(self.departureButton.frame) + 10), (self.placeContainerView.frame.size.width - 20), 60);
     self.arrivalButton.backgroundColor = [UIColor.lightGrayColor colorWithAlphaComponent:0.3];
+    self.arrivalButton.layer.cornerRadius = 4;
     [self.arrivalButton addTarget:self action:@selector(placeButtonDidTap:) forControlEvents:UIControlEventTouchUpInside];
     [self.arrivalButton setEnabled:NO];
-    [self.view addSubview:self.arrivalButton];
+    [self.placeContainerView addSubview:self.arrivalButton];
 }
 
 #pragma mark - IBAction
