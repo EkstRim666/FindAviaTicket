1fb4ef27b040253392b415f77be2be113c21fe58
Andrey Yusupov <electrooptic@yandex.ru>
2018-12-23 10:51:27 +0700

    Add LocationService

---
+++ FindAviaTicket/LocationService.h
@@ -0,0 +1,20 @@
+//
+//  LocationService.h
+//  FindAviaTicket
+//
+//  Created by Andrey Yusupov on 22/12/2018.
+//  Copyright © 2018 Andrey Yusupov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+#define locationServiceDidUpdateCurrentLocation @"LocationServiceDidUpdateCurrentLocation"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
