d6b6b2c22bcaa7f9d6f34cc2cd938b8f714c9377
Andrey Yusupov <electrooptic@yandex.ru>
2018-12-27 21:55:20 +0700

    Add searchController in PlaceTableViewController

--- FindAviaTicket/PlaceTableViewController.m
+++ FindAviaTicket/PlaceTableViewController.m
@@ -13,11 +13,14 @@
 
 #define dataManager [DataManager sharedInstance]
 
-@interface PlaceTableViewController ()
+@interface PlaceTableViewController () <UISearchResultsUpdating>
 
 @property (assign, nonatomic) PlaceType placeType;
 @property (strong, nonatomic) UISegmentedControl *navigationSegmentedControl;
+@property (strong, nonatomic) UISearchController *searchController;
 @property (strong, nonatomic) NSArray *dataArray;
+@property (strong, nonatomic) NSArray *searchArray;
+@property (strong, nonatomic) NSArray *resultArray;
 
 @end
 
@@ -40,6 +43,16 @@
 #pragma mark - PrepareUI
 -(void)prepareUI {
     self.navigationController.navigationBar.tintColor = UIColor.blackColor;
+    
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.dimsBackgroundDuringPresentation = NO;
+    self.searchController.searchResultsUpdater = self;
+    if (@available(iOS 11.0, *)) {
+        self.navigationItem.searchController = self.searchController;
+        self.navigationItem.hidesSearchBarWhenScrolling = NO;
+    } else {
+        self.tableView.tableHeaderView = self.searchController.searchBar;
+    }
     
     self.navigationSegmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Cities", @"Airports"]];
     self.navigationSegmentedControl.tintColor = UIColor.blackColor;
@@ -77,7 +90,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.dataArray.count;
+    return self.resultArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -88,14 +101,14 @@
     
     switch (self.navigationSegmentedControl.selectedSegmentIndex) {
         case 0: {
-            City *city = [self.dataArray objectAtIndex:indexPath.row];
+            City *city = [self.resultArray objectAtIndex:indexPath.row];
             cell.textLabel.text = city.name;
             cell.detailTextLabel.text = city.code;
             break;
         }
         case 1:
         default: {
-            Airport *airport = [self.dataArray objectAtIndex:indexPath.row];
+            Airport *airport = [self.resultArray objectAtIndex:indexPath.row];
             cell.textLabel.text = airport.name;
             cell.detailTextLabel.text = airport.code;
             break;
@@ -107,8 +120,22 @@
 #pragma mark - Table delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     DataSourceType dataType = (int)self.navigationSegmentedControl.selectedSegmentIndex + 1;
-    [self.delegate selectedPlace:[self.dataArray objectAtIndex:indexPath.row] withType:self.placeType andDataType:dataType];
+    [self.delegate selectedPlace:[self.resultArray objectAtIndex:indexPath.row] withType:self.placeType andDataType:dataType];
+    self.searchController.active = NO;
     [self.navigationController popViewControllerAnimated:YES];
 }
 
+#pragma mark - Getter
+-(NSArray *)resultArray {
+    return (self.searchController.isActive && (self.searchArray.count > 0) ? self.searchArray : self.dataArray);
+}
+
+#pragma mark - UISearchResultUpdating
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+    if (searchController.searchBar.text) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self.name CONTAINS[cd] %@", searchController.searchBar.text];
+        self.searchArray = [self.dataArray filteredArrayUsingPredicate:predicate];
+        [self.tableView reloadData];
+    }
+}
 @end
